3908ecf80e5887f050e7b47f7077f7630b8af6e3
Bhargav Ponnapalli <imbhargav5@gmail.com>
2023-05-17 18:59:53 +0530

    minor updates

--- src/app/(authenticated-pages)/(user-pages)/project/[projectId]/(specific-project-pages)/page.tsx
+++ src/app/(authenticated-pages)/(user-pages)/project/[projectId]/(specific-project-pages)/page.tsx
@@ -11,8 +11,12 @@ export default function ProjectPage() {
       <T.H3>{projectByIdData.name}</T.H3>
       <T.Subtle>This is your project page. You should create components related to your business use case here. </T.Subtle>
     </div>
-    <div className="grid grid-cols-[1fr,auto]">
-      <div>Project</div>
+    <div className="grid grid-cols-[1fr,auto] gap-6">
+      <div className="border-2 border-blue-500 rounded-md border-dashed h-48 flex justify-center items-center">
+        <p className="text-sm select-none text-gray-500">
+          Build something cool here!
+        </p>
+      </div>
       <ProjectComments />
     </div>
 
