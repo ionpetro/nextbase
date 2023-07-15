7e97d3c09510799351c2c0b926923a664c7337ce
Bhargav Ponnapalli <imbhargav5@gmail.com>
2023-07-16 00:22:15 +0530

    Show blog cover image

--- src/app/(external-pages)/blog/[slug]/page.tsx
+++ src/app/(external-pages)/blog/[slug]/page.tsx
@@ -16,9 +16,18 @@ export default async function BlogPostPage({ params }: { params: unknown }) {
     );
     console.log(post.content);
     return (
-      <div className="prose prose-lg prose-slate  dark:prose-invert prose-headings:font-display font-default focus:outline-none max-w-full">
-        <h1>{post.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+      <div className="relative w-full space-y-8 max-w-4xl mx-auto">
+        {post.cover_image ? (
+          <img
+            src={post.cover_image}
+            alt={post.title}
+            className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+          />
+        ) : null}
+        <div className="prose prose-lg prose-slate  dark:prose-invert prose-headings:font-display font-default focus:outline-none max-w-full">
+          <h1>{post.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+        </div>
       </div>
     );
   } catch (error) {
