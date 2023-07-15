c47972a0fda21a3918cc4fce836ec72b4819477f
Bhargav Ponnapalli <imbhargav5@gmail.com>
2023-07-15 20:17:10 +0530

    working blog page

--- src/app/(external-pages)/blog/(list)/page.tsx
+++ src/app/(external-pages)/blog/(list)/page.tsx
@@ -1,8 +1,6 @@
-import { Anchor } from '@/components/Anchor';
 import { T } from '@/components/ui/Typography';
 import { supabaseUserServerComponentClient } from '@/supabase-clients/user/supabaseUserServerComponentClient';
 import moment from 'moment';
-import { MDXRemote } from 'next-mdx-remote/rsc';
 
 const getAllBlogPosts = async () => {
   const { data, error } = await supabaseUserServerComponentClient
@@ -21,28 +19,63 @@ export default async function BlogListPage() {
   const blogPosts = await getAllBlogPosts();
 
   return (
-    <div className="space-y-4">
-      <T.H1>Blog</T.H1>
-      {/* @ts-expect-error Async Server Component */}
-      <MDXRemote source={`<h1>hellow world</h1>`} />
+    <div className="space-y-8 w-full">
+      <div className="space-y-3">
+        <T.H1>Blog</T.H1>
+        <T.Subtle>
+          Here is a collection of the latest blog posts from the team at
+          Nextbase.
+        </T.Subtle>
+      </div>
       {blogPosts.length ? (
-        <div className="space-y-4">
-          {blogPosts.map((blog) => (
-            <div key={blog.id} className="bg-white shadow rounded-lg p-4">
-              <h2 className="text-xl font-bold mb-2">{blog.title}</h2>
-              <p className="text-gray-600 mb-2">
-                {blog.summary.slice(0, 100)}
-                {blog.summary.length > 100 && '...'}
-              </p>
-              <p className="text-gray-400 text-sm">
-                Published: {moment(blog.created_at).format('MMMM Do, YYYY')}
-              </p>
-
-              <div className="flex items-center justify-between">
-                <Anchor href={`/blog/${blog.slug}`}>Read more</Anchor>
-              </div>
-            </div>
-          ))}
+        <div className="space-y-2">
+          <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            {blogPosts.map((post) => (
+              <article
+                key={post.id}
+                className="flex flex-col items-start justify-between"
+              >
+                <div className="relative w-full">
+                  <img
+                    src={post.cover_image ?? '/images/nextbase-logo.png'}
+                    alt={post.title}
+                    className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+                  />
+                  <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+                </div>
+                <div className="max-w-xl">
+                  <div className="mt-8 flex items-center gap-x-4 text-xs">
+                    <time dateTime={post.created_at} className="text-gray-500">
+                      {moment(post.created_at).format('MMM D, YYYY')}
+                    </time>
+                  </div>
+                  <div className="group relative">
+                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                      <a href={`/blog/${post.slug}`}>
+                        <span className="absolute inset-0" />
+                        {post.title}
+                      </a>
+                    </h3>
+                    <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                      {post.summary}
+                    </p>
+                  </div>
+                  {/* <div className="relative mt-8 flex items-center gap-x-4">
+                <img src={post.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-100" />
+                <div className="text-sm leading-6">
+                  <p className="font-semibold text-gray-900">
+                    <a href={post.author.href}>
+                      <span className="absolute inset-0" />
+                      {post.author.name}
+                    </a>
+                  </p>
+                  <p className="text-gray-600">{post.author.role}</p>
+                </div>
+              </div> */}
+                </div>
+              </article>
+            ))}
+          </div>
         </div>
       ) : (
         <T.Subtle>No blog posts yet.</T.Subtle>
