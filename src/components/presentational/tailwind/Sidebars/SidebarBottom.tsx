b4471a15f52b17267dee3ce0235eb6bcd796cae2
Siddharth Ponnapalli <107192927+psiddharthdesign@users.noreply.github.com>
2023-07-23 12:04:10 +0530

    added the functionality to expand sidebar when clicked on sidebarBottom (#14)
    * added the functionality to expand sidebar when clicked on sidebarBottom
    
    * fixed popover on SidebarBottom

--- src/components/presentational/tailwind/Sidebars/SidebarBottom.tsx
+++ src/components/presentational/tailwind/Sidebars/SidebarBottom.tsx
@@ -1,8 +1,8 @@
-import { Menu } from '@headlessui/react';
 import Image from 'next/image';
 import { UserSidebarMenu } from '../UserSidebarMenu';
 import ChevronRight from 'lucide-react/dist/esm/icons/chevron-right';
 import { cn } from '@/utils/cn';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/Popover';
 
 export const SidebarBottom = ({
   avatarUrl,
@@ -17,33 +17,39 @@ export const SidebarBottom = ({
 }) => {
   const userClassName = cn(
     `flex w-full gap-3 mb-2 items-center py-3 text-white h-[80px] border-t border-slate-600`,
-    isExpanded ? 'px-4 pl-5 justify-start' : 'justify-center',
+    isExpanded ? 'px-4 pl-5 justify-start' : 'px-4 justify-center',
     'group hover:cursor-pointer'
   );
 
   return (
     <div className="cursor-pointer">
-      <Menu as="div" className="relative">
-        <Menu.Button as="div" className={userClassName}>
-          <Image
-            width="32"
-            height="32"
-            src={avatarUrl}
-            className="w-8 h-8 rounded-full object-cover"
-            alt="User avatar"
-          />
-          {isExpanded ? (
-            <div className="flex flex-col w-full">
-              <div className="flex justify-between w-full items-center">
-                <p className="text-sm text-white font-[500]">{userFullname}</p>
-                <ChevronRight className="text-2xl text-white opacity-0 group-hover:opacity-100" />
-              </div>
-              <p className="text-sm text-slate-400">{userEmail}</p>
+      <div className="relative">
+        <Popover>
+          <PopoverTrigger>
+            <div className={userClassName}>
+              <Image
+                width="32"
+                height="32"
+                src={avatarUrl}
+                className="w-8 h-8 rounded-full object-cover"
+                alt="User avatar"
+              />
+              {isExpanded ? (
+                <div className="flex flex-col w-full">
+                  <div className="flex justify-between w-full items-center">
+                    <p className="text-sm text-white font-[500]">{userFullname}</p>
+                    <ChevronRight className="text-2xl text-white opacity-0 group-hover:opacity-100" />
+                  </div>
+                  <p className="text-sm text-slate-400">{userEmail}</p>
+                </div>
+              ) : null}
             </div>
-          ) : null}
-        </Menu.Button>
-        {isExpanded ? <UserSidebarMenu /> : null}
-      </Menu>
+          </PopoverTrigger>
+          <PopoverContent>
+            <UserSidebarMenu />
+          </PopoverContent>
+        </Popover>
+      </div>
     </div>
   );
 };
--- src/components/presentational/tailwind/UserSidebarMenu/UserSidebarMenu.tsx
+++ src/components/presentational/tailwind/UserSidebarMenu/UserSidebarMenu.tsx
@@ -1,5 +1,3 @@
-import { Fragment } from 'react';
-import { Menu, Transition } from '@headlessui/react';
 import { Anchor } from '@/components/Anchor';
 import AccountsIcon from 'lucide-react/dist/esm/icons/users';
 import SecurityIcon from 'lucide-react/dist/esm/icons/shield';
@@ -8,60 +6,39 @@ import { cn } from '@/utils/cn';
 
 export function UserSidebarMenu() {
   return (
-    <Transition
-      as={Fragment}
-      enter="transition ease-out duration-100"
-      enterFrom="transform opacity-0 scale-95"
-      enterTo="transform opacity-100 scale-100"
-      leave="transition ease-in duration-75"
-      leaveFrom="transform opacity-100 scale-100"
-      leaveTo="transform opacity-0 scale-95"
-    >
-      <Menu.Items className="absolute right-8 bottom-12 z-10 mt-2 w-[172px] origin-bottom-left rounded-md bg-white shadow-xl ring-1 ring-black ring-opacity-5 focus:outline-none">
-        <div className="py-1.5">
-          <Menu.Item>
-            {({ active }) => (
-              <Anchor
-                href="/settings"
-                className={cn(
-                  active ? 'bg-slate-100 text-slate-900' : 'text-slate-700',
-                  'flex px-3 gap-2 items-center py-2 text-sm'
-                )}
-              >
-                <AccountsIcon className="text-lg" /> Account settings
-              </Anchor>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Anchor
-                href="/settings/security"
-                className={cn(
-                  active ? 'bg-slate-100 text-slate-900' : 'text-slate-700',
-                  'flex px-3 gap-2 items-center py-2 text-sm'
-                )}
-              >
-                <SecurityIcon className="text-lg" /> Security Settings
-              </Anchor>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Anchor
-                href="/logout"
-                prefetch={false}
-                className={cn(
-                  active ? 'bg-slate-100 text-slate-900' : 'text-slate-700',
-                  'flex px-3 gap-2 items-center py-2 text-sm'
-                )}
-              >
-                <LogoutIcon className="text-lg" />
-                Sign out
-              </Anchor>
-            )}
-          </Menu.Item>
-        </div>
-      </Menu.Items>
-    </Transition>
+
+    <div>
+      <Anchor
+        href="/settings"
+        className={cn(
+          'hover:bg-slate-100 hover:text-slate-900 text-slate-700',
+          'flex px-3 gap-2 items-center py-2 text-sm'
+        )}
+      >
+        <AccountsIcon className="text-lg" /> Account settings
+      </Anchor>
+
+      <Anchor
+        href="/settings/security"
+        className={cn(
+          'hover:bg-slate-100 hover:text-slate-900 text-slate-700',
+          'flex px-3 gap-2 items-center py-2 text-sm'
+        )}
+      >
+        <SecurityIcon className="text-lg" /> Security Settings
+      </Anchor>
+      <Anchor
+        href="/logout"
+        prefetch={false}
+        className={cn(
+          'hover:bg-slate-100 hover:text-slate-900 text-slate-700',
+          'flex px-3 gap-2 items-center py-2 text-sm'
+        )}
+      >
+        <LogoutIcon className="text-lg" />
+        Sign out
+      </Anchor>
+    </div>
+
   );
 }
