324eccc3c1ca88649c77c733da938debe722feee
Bhargav Ponnapalli <imbhargav5@gmail.com>
2023-07-29 23:05:00 +0530

    Add security patch

---
+++ src/app/(authenticated-pages)/(user-pages)/dashboard/actions.ts
@@ -0,0 +1,11 @@
+'use server'
+
+import { getLoggedInUserAction } from "@/app/_server-actions/user";
+import { createSupabaseUserServerActionClient } from "@/supabase-clients/user/createSupabaseUserServerActionClient";
+import { createOrganization } from "@/utils/supabase-queries";
+
+export async function createOrganizationAction(name: string) {
+  const supabaseClient = createSupabaseUserServerActionClient();
+  const user = await getLoggedInUserAction(supabaseClient);
+  return await createOrganization(supabaseClient, user, name);
+}
---
+++ supabase/migrations/20230508155528_security_patch.sql
@@ -0,0 +1,57 @@
+DO $$
+DECLARE r RECORD;
+BEGIN FOR r IN (
+  SELECT rolname
+  FROM pg_roles
+  WHERE rolname <> 'service_role'
+) LOOP EXECUTE format(
+  'REVOKE EXECUTE ON FUNCTION make_user_app_admin FROM %I',
+  r.rolname
+);
+EXECUTE format(
+  'REVOKE EXECUTE ON FUNCTION remove_app_admin_privilege_for_user FROM %I',
+  r.rolname
+);
+EXECUTE format(
+  'REVOKE EXECUTE ON FUNCTION app_admin_get_all_organizations FROM %I',
+  r.rolname
+);
+EXECUTE format(
+  'REVOKE EXECUTE ON FUNCTION app_admin_get_all_users FROM %I',
+  r.rolname
+);
+EXECUTE format(
+  'REVOKE EXECUTE ON FUNCTION app_admin_get_organizations_created_per_month FROM %I',
+  r.rolname
+);
+EXECUTE format(
+  'REVOKE EXECUTE ON FUNCTION app_admin_get_projects_created_per_month FROM %I',
+  r.rolname
+);
+EXECUTE format(
+  'REVOKE EXECUTE ON FUNCTION app_admin_get_recent_30_day_signin_count FROM %I',
+  r.rolname
+);
+EXECUTE format(
+  'REVOKE EXECUTE ON FUNCTION app_admin_get_total_organization_count FROM %I',
+  r.rolname
+);
+EXECUTE format(
+  'REVOKE EXECUTE ON FUNCTION app_admin_get_total_project_count FROM %I',
+  r.rolname
+);
+EXECUTE format(
+  'REVOKE EXECUTE ON FUNCTION app_admin_get_total_user_count FROM %I',
+  r.rolname
+);
+EXECUTE format(
+  'REVOKE EXECUTE ON FUNCTION app_admin_get_users_created_per_month FROM %I',
+  r.rolname
+);
+EXECUTE format(
+  'REVOKE EXECUTE ON FUNCTION get_app_admin_organizations_created_per_month FROM %I',
+  r.rolname
+);
+END LOOP;
+END;
+$$;
