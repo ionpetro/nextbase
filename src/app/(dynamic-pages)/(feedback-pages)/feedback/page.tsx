97732c22bfad71ffb77ec8b6ed09a7325d7fb1ce
JayMGurav <jaymgruav@gmail.com>
2024-04-09 12:15:57 +0530

    feat(nex-99): add grid for feedback area

--- src/app/(dynamic-pages)/(feedback-pages)/feedback/page.tsx
+++ src/app/(dynamic-pages)/(feedback-pages)/feedback/page.tsx
@@ -17,24 +17,25 @@ export default async function FeedbackListPage({
     const totalPages = await getInternalFeedbackTotalPages(validatedSearchParams);
     const suspenseKey = JSON.stringify(validatedSearchParams);
     return (
-        <div className="space-y-4 max-w-[1296px] mx-auto">
+        <div className="space-y-4 max-w-[1296px] px-2 mx-auto py-4">
             <PageHeading
                 title="Feedback"
                 subTitle="View all feedback your users have submitted."
             ></PageHeading>
-            <div className="flex space-x-4">
+            <div className="flex h-[50vh] items-stretch space-x-4">
                 <div className='flex-1 flex flex-col gap-2'>
                     <div className='flex flex-col gap-2'>
                         <Search placeholder="Search Feedback... " />
                         <FeedbackFacetedFilters />
                     </div>
-                    <Separator orientation="horizontal" />
-                    <div className='h-40 border mt-4'>
+                    <div className='h-40 border rounded-md p-2 mt-4'>
                         Feelback list
                     </div>
                 </div>
-                <Separator orientation="vertical" />
-                <div className='hidden md:flex flex-1 border rounded-lg p-4'>First issue</div>
+                <Separator orientation="vertical" className='hidden md:block self-stretch' />
+                <div className='hidden md:flex flex-1 border rounded-lg p-4 h-80'>
+                    First Feedback
+                </div>
             </div>
         </div>
     );
