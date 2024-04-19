512deb1d472589a19b8cdb043e63ea2e41049082
next-base-user <nextbaseuser@gmail.com>
2024-04-19 18:51:51 -0300

    fix(NEX-97) Roadmap kanban and changelogs feature

--- next.config.mjs
+++ next.config.mjs
@@ -67,6 +67,7 @@ const withMDX = createWithMdx({
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   images: {
+    domains: ['localhost'],
     remotePatterns: [
       {
         protocol: 'https',
