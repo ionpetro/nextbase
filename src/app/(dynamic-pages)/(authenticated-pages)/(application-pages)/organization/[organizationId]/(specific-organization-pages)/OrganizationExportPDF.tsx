1a107f612c6ba022d381eec296bc17514162eb46
next-base-user <nextbaseuser@gmail.com>
2024-04-26 10:21:12 -0300

    fix(UI): PDF exporting awaiting canvas loading

--- src/app/(dynamic-pages)/(authenticated-pages)/(application-pages)/organization/[organizationId]/(specific-organization-pages)/OrganizationExportPDF.tsx
+++ src/app/(dynamic-pages)/(authenticated-pages)/(application-pages)/organization/[organizationId]/(specific-organization-pages)/OrganizationExportPDF.tsx
@@ -7,13 +7,15 @@ import { useTheme } from 'next-themes';
 import { useState } from 'react';
 export const OrganizationExportPDF = () => {
   const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(false);
 
   const { theme } = useTheme()
 
   const exportPDF = () => {
     const input = document.getElementById("export-container");
 
-    if (input) {
+    if (input && input.offsetHeight > 0 && input.offsetWidth > 0) {
+      setDisabled(true);
       setLoading(true);
       html2canvas(input, {
         scale: 2, backgroundColor: theme === "dark" ? "#020817" : "#fff",
@@ -31,13 +33,17 @@ export const OrganizationExportPDF = () => {
           pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
           pdf.save("organization.pdf");
         })
-        .finally(() => setLoading(false));
+        .finally(() => {
+          setDisabled(false);
+          setLoading(false);
+        });
     } else {
+      setDisabled(true);
       setLoading(false);
     }
   };
   return (
-    <Button variant={"outline"} onClick={exportPDF} className="mb-4 flex gap-4 items-center" disabled={loading}>
+    <Button variant={"outline"} onClick={exportPDF} className="mb-4 flex gap-4 items-center" disabled={disabled}>
       <Upload className='size-4' />
       {loading ? "Exporting..." : "Export PDF"}
     </Button>
